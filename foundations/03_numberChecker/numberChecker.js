5688f9f6cacd33008eeaa2d324af039abe2445b5
r05t02 <256002142+r05t02@users.noreply.github.com>
2026-04-07 08:40:26 +0530

    fix conditionals to return true

--- foundations/03_numberChecker/numberChecker.js
+++ foundations/03_numberChecker/numberChecker.js
@@ -1,10 +1,19 @@
+// function numberChecker(number) {
+//   if (number === 6) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
+// ---------------OR----------------
 function numberChecker(number) {
-  if (number === 6) {
+  if (number >= 10) {
     return true;
   } else {
     return false;
   }
 }
+
 
 // Do not edit below this line
 module.exports = numberChecker;
