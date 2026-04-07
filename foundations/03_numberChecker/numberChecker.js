db8a8450d168921fe1e7ca6da4d1d1387fdd50f5
r05t02 <256002142+r05t02@users.noreply.github.com>
2026-04-07 12:51:13 +0530

    03_numberChecker --> All 4 tests pass

--- foundations/03_numberChecker/numberChecker.js
+++ foundations/03_numberChecker/numberChecker.js
@@ -14,6 +14,13 @@ function numberChecker(number) {
   }
 }
 
+// function numberChecker(number) {
+//   if (number < 10) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
 
 // Do not edit below this line
 module.exports = numberChecker;
